d4729715699228c98a0e65a71ef96f896cd97acd
auchan <auchan129@foxmail.com>
2018-02-10 15:33:58 +0800

    improve copy-to-clipboard support

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -36,44 +36,6 @@ function upload(input) {
     }
 }
 
-       //复制到剪切板js代码
-function copyToClipBoard(s) {
-    //alert(s);
-    if (window.clipboardData) {
-        window.clipboardData.setData("Text", s);
-        alert("已经复制到剪切板！"+ "\n" + s);
-    } else if (navigator.userAgent.indexOf("Opera") != -1) {
-        window.location = s;
-    } else if (window.netscape) {
-        try {
-            netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-        } catch (e) {
-            alert("被浏览器拒绝！\n请在浏览器地址栏输入'about:config'并回车\n然后将'signed.applets.codebase_principal_support'设置为'true'");
-        }
-        var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard);
-        if (!clip)
-            return;
-        var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable);
-        if (!trans)
-            return;
-        trans.addDataFlavor('text/unicode');
-        var str = new Object();
-        var len = new Object();
-        var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
-        var copytext = s;
-        str.data = copytext;
-        trans.setTransferData("text/unicode", str, copytext.length * 2);
-        var clipid = Components.interfaces.nsIClipboard;
-        if (!clip)
-            return false;
-        clip.setData(trans, null, clipid.kGlobalClipboard);
-        alert("已经复制到剪切板！" + "\n" + s);
-	}
-	else {
-		alert("浏览器不支持！点击编辑器工具栏最右边的“√”按钮，然后手动进行复制");
-	}
-}
-
 $("#BtnSave").bind("click", function (e) {
 	if (typeof Blob != "undefined") {
 		var content = $("#input").GetEditorContent();
@@ -87,7 +49,20 @@ $("#BtnSave").bind("click", function (e) {
 $("#BtnFile").bind("click", function (e) {
 	this.value = "";
 })
-$("#BtnCopy").bind("click", function (e) {
-	var content = $("#input").GetEditorContent();
-	copyToClipBoard(content);
-})
+
+//复制到剪切板功能
+{
+    var clipboard = new Clipboard("#BtnCopy", {
+        text: function() {
+            return $("#input").GetEditorContent();
+        }
+    });
+
+    clipboard.on('success', function(e) {
+        alert("已经复制到剪切板！"+ "\n" + e.text);
+    });
+
+    clipboard.on('error', function(e) {
+        alert("浏览器不支持！点击编辑器工具栏最右边的“√”按钮，然后手动进行复制");
+    });
+}
